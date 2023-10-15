ee4e61f7319767dd48ca31b494fcabd7db17ce80
Jan-Jelle Kester <janjelle@jjkester.nl>
2023-10-15 18:20:36 +0200

    Improve demo app
    Reduce flickering when navigating and avoid drawing under cutouts

--- crt-demo/src/main/kotlin/nl/jjkester/crt/demo/DemoActivity.kt
+++ crt-demo/src/main/kotlin/nl/jjkester/crt/demo/DemoActivity.kt
@@ -3,8 +3,16 @@ package nl.jjkester.crt.demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import nl.jjkester.crt.demo.main.MainNavigation
 import nl.jjkester.crt.demo.markdown.MarkdownShowcase
 
@@ -21,9 +29,13 @@ class DemoActivity : ComponentActivity() {
             }
 
             DemoTheme {
-                MainNavigation(
-                    showcases = showcases
-                )
+                Scaffold(modifier = Modifier.safeDrawingPadding()) { contentPadding ->
+                    Box(modifier = Modifier.padding(contentPadding)) {
+                        MainNavigation(
+                            showcases = showcases
+                        )
+                    }
+                }
             }
         }
     }
